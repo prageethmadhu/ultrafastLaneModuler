7cf33adf3df493e4cf2d41dd871de576216df4a8
prageethmadhu <prageeth.madhu@gmail.com>
2025-01-21 22:29:35 +0530

    minimal working

--- newAttempt/utils/common.py
+++ newAttempt/utils/common.py
@@ -1,8 +1,6 @@
-import os, argparse
-from utils.dist_utils import is_main_process, dist_print, DistSummaryWriter
+import argparse
+from utils.dist_utils import dist_print
 from utils.config import Config
-import torch
-import time
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -63,55 +61,4 @@ def merge_config():
     return args, cfg
 
 
-def save_model(net, optimizer, epoch,save_path, distributed):
-    if is_main_process():
-        model_state_dict = net.state_dict()
-        state = {'model': model_state_dict, 'optimizer': optimizer.state_dict()}
-        # state = {'model': model_state_dict}
-        assert os.path.exists(save_path)
-        model_path = os.path.join(save_path, 'ep%03d.pth' % epoch)
-        torch.save(state, model_path)
 
-import pathspec
-
-def cp_projects(auto_backup, to_path):
-    if is_main_process() and auto_backup:
-        with open('./.gitignore','r') as fp:
-            ign = fp.read()
-        ign += '\n.git'
-        spec = pathspec.PathSpec.from_lines(pathspec.patterns.GitWildMatchPattern, ign.splitlines())
-        all_files = {os.path.join(root,name) for root,dirs,files in os.walk('./') for name in files}
-        matches = spec.match_files(all_files)
-        matches = set(matches)
-        to_cp_files = all_files - matches
-        dist_print('Copying projects to '+ to_path + ' for backup')
-        t0 = time.time()
-        warning_flag = True
-        for f in to_cp_files:
-            dirs = os.path.join(to_path,'code',os.path.split(f[2:])[0])
-            if not os.path.exists(dirs):
-                os.makedirs(dirs)
-            os.system('cp %s %s'%(f,os.path.join(to_path,'code',f[2:])))
-            elapsed_time = time.time() - t0
-            if elapsed_time > 5 and warning_flag:
-                dist_print('If the program is stuck, it might be copying large files in this directory. please don\'t set --auto_backup. Or please make you working directory clean, i.e, don\'t place large files like dataset, log results under this directory.')
-                warning_flag = False
-
-
-
-
-import datetime, os
-def get_work_dir(cfg):
-    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    hyper_param_str = '_lr_%1.0e_b_%d' % (cfg.learning_rate, cfg.batch_size)
-    work_dir = os.path.join(cfg.log_path, now + hyper_param_str + cfg.note)
-    return work_dir
-
-def get_logger(work_dir, cfg):
-    logger = DistSummaryWriter(work_dir)
-    config_txt = os.path.join(work_dir, 'cfg.txt')
-    if is_main_process():
-        with open(config_txt, 'w') as fp:
-            fp.write(str(cfg))
-
-    return logger
